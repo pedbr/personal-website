f05937fcacdf9a544c76b26d8faf769499f17568
Pedro <pedro.bras@getflowbox.com>
2022-02-19 11:10:56 +0100

    adds basic 3D element

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Suspense } from 'react'
+
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+import AnimatedSphere from './components/AnimatedSphere'
 
 function App() {
-  return <div className='App'>Hello World</div>
+  return (
+    <div
+      style={{
+        height: '100vh',
+        marginLeft: '30rem',
+      }}
+    >
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} />
+        <Suspense fallback={null}>
+          <AnimatedSphere />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
 }
 
 export default App
